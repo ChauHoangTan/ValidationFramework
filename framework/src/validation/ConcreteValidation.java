dc75fff7f7feb2a1641187520aa32593efe86b17
anhzo12 <20127439clc1@gmail.com>
2023-12-17 00:35:45 +0700

    ?

---
+++ framework/src/validation/ConcreteValidation.java
@@ -0,0 +1,23 @@
+package validation;
+
+public class ConcreteValidation {
+    private Validation validattion; 
+    private String value;
+    private String reason;
+
+    public ConcreteValidation (Validation validation, String value, String reason){
+        this.validattion = validation;
+    }
+
+    protected String getValue(){
+        return value;
+    }
+
+    protected String getReason(){
+        return reason;
+    }
+
+    protected boolean isValid(){
+        return validattion.isValid(value);
+    }
+}
